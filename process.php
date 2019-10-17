3676cad0202965b4d223e5c4b8ddfadda7f65af1
gazi.gerdeci@gmail.com <gazi.gerdeci@gmail.com>
2019-10-17 14:49:59 +0200

    forth commit

--- process.php
+++ process.php
@@ -3,9 +3,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {//Check it is comming from a form
 
 	//mysql credentials
 	$mysql_host = "172.30.3.205";
-	$mysql_username = "root";
-	$mysql_password = "";
-	$mysql_database = "test";
+	$mysql_username = "user1";
+	$mysql_password = "user123";
+	$mysql_database = "process";
 	
 	$u_name = filter_var($_POST["user_name"], FILTER_SANITIZE_STRING); //set PHP variables like this so we can use them anywhere in code below
 	$u_email = filter_var($_POST["user_email"], FILTER_SANITIZE_EMAIL);
